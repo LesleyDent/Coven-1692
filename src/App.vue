6b08b5ad684ac671b643823dd6c46daa4b7e2de2
Lesley Dent <lesleyjdent@gmail.com>
2025-03-20 19:25:11 -0400

    yep

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="">Coven 1692</h1>
+  </div>
+  <p>Coming soon.</p>
+</template>
+
+<style>
+* {
+  font-family: "Amarante", serif;
+  color: #f1e28f;
+  
+}
+body {
+  background-color: #120422;
+}
+</style>
